9b6e9bde9b918196ada394cd6dd63c40a5f2644f
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-18 19:03:53 -0300

    feat: add term schema

---
+++ lib/rh/term.ex
@@ -0,0 +1,27 @@
+defmodule Rh.Term do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Rh.Company
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  @required_params [:name, :description, :company_id]
+
+  schema "terms" do
+    field :name, :string
+    field :description, :string
+
+    belongs_to :company, Company
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> unique_constraint([:name])
+  end
+end
