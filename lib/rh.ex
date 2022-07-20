8b399736cdcea8ff52878f41169244c28a48df89
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-20 20:27:20 -0300

    feat: add companies crud

--- lib/rh.ex
+++ lib/rh.ex
@@ -1,9 +1,11 @@
 defmodule Rh do
-  @moduledoc """
-  Rh keeps the contexts that define your domain
-  and business logic.
+  alias Rh.Companies.Create, as: CreateCompany
+  alias Rh.Companies.Get, as: GetCompany
+  alias Rh.Companies.Delete, as: DeleteCompany
+  alias Rh.Companies.List, as: ListCompanies
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  defdelegate create_company(params), to: CreateCompany, as: :call
+  defdelegate get_company(id), to: GetCompany, as: :by_id
+  defdelegate delete_company(id), to: DeleteCompany, as: :call
+  defdelegate list_companies, to: ListCompanies, as: :call
 end
