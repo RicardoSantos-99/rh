4c5e5ea6fd4972141e9d48e2873c3e5bd3e13fcf
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-22 14:07:09 -0300

    feat: check company exist before insert affiliate

--- lib/rh/affiliates/create.ex
+++ lib/rh/affiliates/create.ex
@@ -1,8 +1,27 @@
 defmodule Rh.Affiliates.Create do
   alias Rh.Repo
-  alias Rh.Schema.Affiliate
+  alias Rh.Schema.{Affiliate, Company}
 
-  def call(params) do
+  alias Ecto.UUID
+
+  def call(%{name: name, cnpj: cnpj, company_id: company_id}) do
+    params = %{name: name, cnpj: cnpj, company_id: company_id}
+
+    company_id
+    |> UUID.cast()
+    |> handle_response(params)
+  end
+
+  defp handle_response(:error, _params), do: {:error, "Invalid UUID"}
+
+  defp handle_response({:ok, company_id}, params) do
+    case Repo.get(Company, company_id) do
+      nil -> {:error, "Company not found"}
+      _company -> handle_insert(params)
+    end
+  end
+
+  defp handle_insert(params) do
     params
     |> Affiliate.changeset()
     |> Repo.insert()
