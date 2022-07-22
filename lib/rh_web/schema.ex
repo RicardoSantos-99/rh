5a6d5418a01170325d413c6d645038ef8df060d3
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-22 12:52:13 -0300

    feat: add department and cost center

--- lib/rh_web/schema.ex
+++ lib/rh_web/schema.ex
@@ -15,15 +15,27 @@ defmodule RhWeb.Schema do
   import_types RhWeb.Mutations.Affiliate
   import_types RhWeb.Queries.Affiliate
 
+  import_types RhWeb.Schema.Types.Department
+  import_types RhWeb.Mutations.Department
+  import_types RhWeb.Queries.Department
+
+  import_types RhWeb.Schema.Types.CostCenter
+  import_types RhWeb.Mutations.CostCenter
+  import_types RhWeb.Queries.CostCenter
+
   query do
     import_fields :company_queries
     import_fields :occupation_queries
     import_fields :affiliate_queries
+    import_fields :department_queries
+    import_fields :cost_center_queries
   end
 
   mutation do
     import_fields :company_mutation
     import_fields :occupation_mutation
     import_fields :affiliate_mutation
+    import_fields :department_mutation
+    import_fields :cost_center_mutation
   end
 end
