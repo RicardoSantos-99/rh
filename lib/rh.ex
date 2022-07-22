ec58ba6b72bd57fbcf2cc6f8e7d7d8e4b0443019
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-22 11:42:59 -0300

    feat: add department and cost center functions

--- lib/rh.ex
+++ lib/rh.ex
@@ -14,6 +14,16 @@ defmodule Rh do
   alias Rh.Affiliates.Delete, as: DeleteAffiliate
   alias Rh.Affiliates.List, as: ListAffiliates
 
+  alias Rh.CostCenters.Create, as: CreateCostCenters
+  alias Rh.CostCenters.Get, as: GetCostCenters
+  alias Rh.CostCenters.Delete, as: DeleteCostCenters
+  alias Rh.CostCenters.List, as: ListCostCenters
+
+  alias Rh.Departments.Create, as: CreateDepartments
+  alias Rh.Departments.Get, as: GetDepartment
+  alias Rh.Departments.Delete, as: DeleteDepartment
+  alias Rh.Departments.List, as: ListDepartments
+
   defdelegate create_company(params), to: CreateCompany, as: :call
   defdelegate get_company(id), to: GetCompany, as: :by_id
   defdelegate get_company_by_cnpj(cnpj), to: GetCompany, as: :by_cnpj
@@ -29,4 +39,16 @@ defmodule Rh do
   defdelegate get_affiliate(id), to: GetAffiliate, as: :by_id
   defdelegate delete_affiliate(id), to: DeleteAffiliate, as: :call
   defdelegate list_affiliates, to: ListAffiliates, as: :call
+
+  defdelegate create_cost_center(params), to: CreateCostCenters, as: :call
+  defdelegate get_cost_center(id), to: GetCostCenters, as: :by_id
+  defdelegate get_cost_center_by_code(id), to: GetCostCenters, as: :get_by_code
+  defdelegate delete_cost_center(id), to: DeleteCostCenters, as: :call
+  defdelegate list_cost_centers, to: ListCostCenters, as: :call
+
+  defdelegate create_department(params), to: CreateDepartments, as: :call
+  defdelegate get_department(id), to: GetDepartment, as: :by_id
+  defdelegate get_department_by_code(code), to: GetDepartment, as: :get_by_code
+  defdelegate delete_department(id), to: DeleteDepartment, as: :call
+  defdelegate list_departments, to: ListDepartments, as: :call
 end
