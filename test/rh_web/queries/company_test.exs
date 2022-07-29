319b93a1eb44e606001badfd5e7d98e1dd76b99a
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-29 20:50:58 -0300

    test: company queries test

--- test/rh_web/queries/company_test.exs
+++ test/rh_web/queries/company_test.exs
@@ -1,5 +1,8 @@
 defmodule RhWeb.Queries.CompanyTest do
+  @moduledoc false
   use RhWeb.ConnCase, async: true
+
+  import Rh.Factory
 
   @get_company_by_id """
     query getCompany($id: UUID4!) {
@@ -12,8 +15,99 @@ defmodule RhWeb.Queries.CompanyTest do
     }
   """
 
+  @get_company_by_cnpj """
+    query getCompanyByCnpj($cnpj: String!) {
+      getCompanyByCnpj(cnpj: $cnpj) {
+        corporateName
+        cnpj
+        name
+        id
+      }
+    }
+  """
+
+  @list_companies """
+    query listCompanies {
+      listCompanies {
+        corporateName
+        cnpj
+        name
+        id
+      }
+    }
+  """
+
   describe "Company queries" do
-    setup %{} do
+    setup %{conn: conn} do
+      company_params = build(:company)
+
+      {:ok, company} = Rh.create_company(company_params)
+
+      {:ok, conn: conn, company: company}
+    end
+
+    test "find company by id, return company", %{conn: conn, company: %{id: id}} do
+      response =
+        conn
+        |> post("/api/graphql", %{
+          "query" => @get_company_by_id,
+          "variables" => %{id: id}
+        })
+        |> json_response(:ok)
+
+      assert response == %{
+               "data" => %{
+                 "getCompany" => %{
+                   "cnpj" => "12312312312312",
+                   "corporateName" => "RH",
+                   "id" => id,
+                   "name" => "ninho de camundangas"
+                 }
+               }
+             }
+    end
+
+    test "find company by cnpj, return company", %{conn: conn, company: %{cnpj: cnpj, id: id}} do
+      response =
+        conn
+        |> post("/api/graphql", %{
+          "query" => @get_company_by_cnpj,
+          "variables" => %{cnpj: cnpj}
+        })
+        |> json_response(:ok)
+
+      assert response == %{
+               "data" => %{
+                 "getCompanyByCnpj" => %{
+                   "cnpj" => "12312312312312",
+                   "corporateName" => "RH",
+                   "id" => id,
+                   "name" => "ninho de camundangas"
+                 }
+               }
+             }
+    end
+
+    test "find all companies, return a list of all companies", %{conn: conn, company: %{id: id}} do
+      response =
+        conn
+        |> post("/api/graphql", %{
+          "query" => @list_companies
+        })
+        |> json_response(:ok)
+
+      assert response == %{
+               "data" => %{
+                 "listCompanies" => [
+                   %{
+                     "cnpj" => "12312312312312",
+                     "corporateName" => "RH",
+                     "id" => id,
+                     "name" => "ninho de camundangas"
+                   }
+                 ]
+               }
+             }
     end
   end
 end
