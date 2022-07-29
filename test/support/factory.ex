cada700b24ffbeef2cd9daf47a5414ac65b82e59
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-29 20:51:09 -0300

    test: refactor factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,19 +1,25 @@
-defmodule Delivery.Factory do
+defmodule Rh.Factory do
   @moduledoc false
 
   use ExMachina.Ecto, repo: Delivery.Repo
-
-  alias Rh.Schema.Company
 
   def company_params_factory do
     %{cnpj: "12312312312312", corporate_name: "RH", name: "ninho de camundangas"}
   end
 
-  def user_factory do
-    %Company{
+  def company_factory do
+    %{
       cnpj: "12312312312312",
       corporate_name: "RH",
       name: "ninho de camundangas"
+    }
+  end
+
+  def affiliated_factory do
+    %{
+      name: "vli",
+      cnpj: "12341234123412",
+      company_id: Ecto.UUID.generate()
     }
   end
 end
