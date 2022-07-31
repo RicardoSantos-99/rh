f8b5d5ae270a2cabbde9547679f43d25dbe70a89
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-31 20:11:49 -0300

    test: add test for affiliate queries

---
+++ test/rh_web/queries/affiliated_test.exs
@@ -0,0 +1,75 @@
+defmodule RhWeb.Queries.AffiliatedTest do
+  @moduledoc false
+  use RhWeb.ConnCase, async: true
+
+  import Rh.Factory
+
+  @get_affiliate_by_id """
+    query getAffiliate($id: UUID4!) {
+      getAffiliate(id: $id) {
+        id
+        cnpj
+        name
+      }
+    }
+  """
+
+  @get_affiliates """
+    query listAffiliates {
+      listAffiliates {
+        cnpj
+        name
+      }
+    }
+  """
+
+  describe "affiliate queries" do
+    setup %{conn: conn} do
+      company_params = build(:company)
+
+      {:ok, company} = Rh.create_company(company_params)
+
+      {:ok, affiliate} =
+        build(:affiliated, %{company_id: company.id})
+        |> Rh.create_affiliate()
+
+      {:ok, conn: conn, affiliate: affiliate}
+    end
+
+    test "find affiliate by id, return affiliate", %{conn: conn, affiliate: %{id: id, cnpj: cnpj}} do
+      response =
+        conn
+        |> post("/api/graphql", %{
+          "query" => @get_affiliate_by_id,
+          "variables" => %{id: id}
+        })
+        |> json_response(:ok)
+
+      assert response == %{
+               "data" => %{
+                 "getAffiliate" => %{
+                   "id" => id,
+                   "name" => "vli",
+                   "cnpj" => cnpj
+                 }
+               }
+             }
+    end
+
+    test "find all affiliate, should return a list of affiliates", %{
+      conn: conn,
+      affiliate: %{cnpj: cnpj}
+    } do
+      response =
+        conn
+        |> post("/api/graphql", %{
+          "query" => @get_affiliates
+        })
+        |> json_response(:ok)
+
+      assert response == %{
+               "data" => %{"listAffiliates" => [%{"cnpj" => cnpj, "name" => "vli"}]}
+             }
+    end
+  end
+end
