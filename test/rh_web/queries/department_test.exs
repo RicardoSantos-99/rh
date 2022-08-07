6422c297278f046b19b65684b8e66f57d5675083
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-08-07 19:08:19 -0300

    test: add department test

---
+++ test/rh_web/queries/department_test.exs
@@ -0,0 +1,18 @@
+defmodule RhWeb.Queries.DepartmentTest do
+  @moduledoc false
+  use RhWeb.ConnCase, async: true
+
+  describe "departments queries" do
+    test "find departments by id, return departments" do
+      response = true
+
+      assert response == true
+    end
+
+    test "find all departments, should return a list of departments" do
+      response = true
+
+      assert response == true
+    end
+  end
+end
