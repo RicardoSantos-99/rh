1297793123d8e4bef06b6ec049cb2477463bffc3
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-08-12 20:00:28 -0300

    fix: set one for one strategy in supervisor

--- lib/rh/application.ex
+++ lib/rh/application.ex
@@ -24,7 +24,7 @@ defmodule Rh.Application do
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :man, name: Rh.Supervisor]
+    opts = [strategy: :one_for_one, name: Rh.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
