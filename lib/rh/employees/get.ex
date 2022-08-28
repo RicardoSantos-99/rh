25d5a7f2b7830e28a34c20b6966e80fba3d77d67
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-08-28 19:15:29 -0300

    feat: get employee with auth

--- lib/rh/employees/get.ex
+++ lib/rh/employees/get.ex
@@ -1,25 +1,24 @@
 defmodule Rh.Employees.Get do
-  alias Rh.Repositories.EmployeeRepository
+  import Rh.Repositories.EmployeeRepository
 
   alias Ecto.UUID
+  alias Rh.Schema.Employee
+  alias Rh.Utils.Auth
 
-  def by_id(id, _current_user) do
-    id
-    |> UUID.cast()
-    |> handle_response()
-  end
-
-  def by_email(email, _current_user) do
-    case EmployeeRepository.find_employee_by_email(email) do
-      nil -> {:error, "Employee not found"}
-      employee -> {:ok, employee}
+  def by_id(employee_id, current_user) do
+    with %Employee{company_id: company_id} = employee <- find_employee_by_id(employee_id),
+         {:ok, _id} <- Auth.check_access(company_id, current_user, :RECRUIT),
+         {:ok, _uuid} <- UUID.cast(employee_id) do
+      {:ok, employee}
+    else
+      nil -> {:error, "Affiliate not found"}
+      {:error, message} -> {:error, message}
+      :error -> {:error, "Invalid UUID"}
     end
   end
 
-  defp handle_response(:error), do: {:error, "Invalid UUID"}
-
-  defp handle_response({:ok, id}) do
-    case EmployeeRepository.find_employee_by_id(id) do
+  def by_email(email, _current_user) do
+    case find_employee_by_email(email) do
       nil -> {:error, "Employee not found"}
       employee -> {:ok, employee}
     end
